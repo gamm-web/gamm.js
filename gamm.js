f71b4e2ff9c57c40f951f27ed06f46280c9f465e
gamm-web <prefikes@gmail.com>
2021-03-24 14:17:36 +0800

    add license

--- gamm.js
+++ gamm.js
@@ -1,7 +1,9 @@
 /*
-Author: Angelo Octavio
-Version: 1.1
-Author Url: gamm.website/gammjs
+* Author: Angelo Octavio
+ * Version: 1.1
+ * Author Url: http://gamm.website/gammjs
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
 window.GAMM_IDS = [];
